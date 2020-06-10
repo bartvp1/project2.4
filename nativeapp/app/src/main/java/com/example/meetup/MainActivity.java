17b7e4a27dadc4c2d2f59b2e62598d8db5d3676e
Bart <bartvp@live.nl>
2020-06-10 19:20:24 +0200

    bottom navigation bar + action bar

--- nativeapp/app/src/main/java/com/example/meetup/MainActivity.java
+++ nativeapp/app/src/main/java/com/example/meetup/MainActivity.java
@@ -3,65 +3,115 @@ package com.example.meetup;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
-import com.google.android.material.tabs.TabLayout;
-import androidx.viewpager.widget.ViewPager;
-import androidx.appcompat.app.AppCompatActivity;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
-import com.example.meetup.ui.main.SectionsPagerAdapter;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
+
+import com.example.meetup.ui.main.fragments.MatchesFragment;
+import com.example.meetup.ui.main.fragments.NotificationsFragment;
+import com.example.meetup.ui.main.fragments.ProfileFragment;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MainActivity extends AppCompatActivity {
     PrefsManager prefmanager;
-    private Thread loginchecker;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        prefmanager=PrefsManager.getInstance(this.getApplicationContext());
-        Runnable checklogin=()->{
-            while (isLoggedIn(prefmanager)){}
-            this.runOnUiThread(new Runnable() {
-                                   @Override
-                                   public void run() {
-                                       if(prefmanager.getToken()!=null) {
-                                           logOut();
-                                       }
-                                   }
-                               });
 
-        };
-        loginchecker=new Thread(checklogin);
-        loginchecker.start();
-
+        checkLogin();
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        SectionsPagerAdapter sectionsPagerAdapter = new SectionsPagerAdapter(this, getSupportFragmentManager());
-        ViewPager viewPager = findViewById(R.id.view_pager);
-        viewPager.setAdapter(sectionsPagerAdapter);
-        TabLayout tabs = findViewById(R.id.tabs);
-        tabs.setupWithViewPager(viewPager);
+
+        BottomNavigationView nav = findViewById(R.id.navigation_view);
+        nav.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
 
     }
-    public void logOutClick(View view){
-      logOut();
+
+    @Override
+    public boolean onPrepareOptionsMenu(Menu menu) {
+        return super.onPrepareOptionsMenu(menu);
     }
-    public void logOut(){
-        this.runOnUiThread(()-> {
-            SharedPreferences.Editor editor=prefmanager.getEditor();
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater menuInflater = getMenuInflater();
+        menuInflater.inflate(R.menu.main_menu, menu);
+        return true;
+    }
+
+    public void checkLogin() {
+        prefmanager = PrefsManager.getInstance(this.getApplicationContext());
+        Runnable checklogin = () -> {
+            while (isLoggedIn(prefmanager)) {
+            }
+            this.runOnUiThread(() -> {
+                if (prefmanager.getToken() != null) {
+                    logOut();
+                }
+            });
+        };
+
+        Thread loginchecker = new Thread(checklogin);
+        loginchecker.start();
+    }
+
+    public void logOutClick(View view) {
+        logOut();
+    }
+
+    public void logOutClick(MenuItem item) {
+        logOut();
+    }
+
+    public void logOut() {
+        this.runOnUiThread(() -> {
+            SharedPreferences.Editor editor = prefmanager.getEditor();
             //set token and expiration
-            editor.putString("token",null);
-            editor.putInt("expiration",0);
+            editor.putString("token", null);
+            editor.putInt("expiration", 0);
             editor.commit();
             Intent intent = new Intent(this, Login.class);
             startActivity(intent);
             finish();
         });
     }
-    public static boolean isLoggedIn(PrefsManager prefmanager){
 
-
-        if(prefmanager.getToken() ==null||( (System.currentTimeMillis()/1000)>prefmanager.getExpiration())){
+    public static boolean isLoggedIn(PrefsManager prefmanager) {
+        if (prefmanager.getToken() == null || ((System.currentTimeMillis() / 1000) > prefmanager.getExpiration())) {
             return false;
         } else {
             return true;
         }
+    }
+
+    private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
+            = item -> {
+        getSupportActionBar().setTitle(item.getTitle());
+        switch (item.getItemId()) {
+            case R.id.botton_notifications:
+                loadFragment(new NotificationsFragment());
+                return true;
+            case R.id.bottom_matches:
+                loadFragment(new MatchesFragment());
+                return true;
+            case R.id.bottom_profile:
+                loadFragment(new ProfileFragment());
+                return true;
+        }
+        return false;
+    };
+
+    private void loadFragment(Fragment fragment) {
+        // load fragment
+        FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+        transaction.replace(R.id.container, fragment);
+        transaction.addToBackStack(null);
+        transaction.commit();
     }
 
 
