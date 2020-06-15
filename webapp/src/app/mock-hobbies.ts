1a59dd6fa697cfa094cd294aa8fd6008859e76ba
Maurice Wijker <mauricewijker@gmail.com>
2020-06-15 15:28:20 +0200

    add function to add new hobby to list

--- webapp/src/app/mock-hobbies.ts
+++ webapp/src/app/mock-hobbies.ts
@@ -1,14 +1,14 @@
 import { Hobby } from './hobby';
 
 export const HOBBIES: Hobby[] = [
-  { id: 11, name: 'Tennis' },
-  { id: 12, name: 'Squash' },
-  { id: 13, name: 'Fitness' },
-  { id: 14, name: 'Inline Skating' },
-  { id: 15, name: 'Jogging' },
-  { id: 16, name: 'Swimming' },
-  { id: 17, name: 'Horse riding' },
-  { id: 18, name: 'Football' },
-  { id: 19, name: 'Gaming' },
+  { name: 'Tennis' },
+  { name: 'Squash' },
+  { name: 'Fitness' },
+  { name: 'Inline Skating' },
+  { name: 'Jogging' },
+  { name: 'Swimming' },
+  { name: 'Horse riding' },
+  { name: 'Football' },
+  { name: 'Gaming' },
 ];
 
